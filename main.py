15e00856b464c360e9245bd47710bc25419aa6da
Fin250 <90973466+Fin250@users.noreply.github.com>
2024-05-05 15:27:18 +0000

    Homepage created

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    request,
+    session,
+    redirect,
+    url_for,
+    g,
+    flash,
+    jsonify,
+)
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello from Flask!'
+def homepage():
+    return render_template("/homepage.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
