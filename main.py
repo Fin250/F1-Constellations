7543669ee3ae9c099b92fd78209fa8828a36bfac
Fin250 <90973466+Fin250@users.noreply.github.com>
2024-05-08 11:35:43 +0000

    Asset update + homepage completion

--- main.py
+++ main.py
@@ -17,5 +17,21 @@ app = Flask(__name__)
 def homepage():
     return render_template("/homepage.html")
 
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/tracks/bahrain')
+def bahrain():
+    return render_template("/tracks/bahrain.html")
+
+@app.route('/tracks/saudi-arabia')
+def saudi_arabia():
+    return render_template("saudi-arabia.html")
+
+@app.route('/tracks/australia.html')
+def australia():
+    return render_template("australia.html")
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
