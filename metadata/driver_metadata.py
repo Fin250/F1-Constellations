8ac0bf201d675df087ce681121a2f0685f38421c
Finlay Sweet <finlaytsweet@gmail.com>
2025-09-23 15:00:00 +0100

    Added metadata for all drivers

--- metadata/driver_metadata.py
+++ metadata/driver_metadata.py
@@ -98,5 +98,225 @@ DRIVER_METADATA = {
     "zhou": {
         "full_name": "Zhou Guanyu",
         "nationality": "China"
+    },
+    "rosberg": {
+        "full_name": "Nico Rosberg",
+        "nationality": "Germany"
+    },
+    "webber": {
+        "full_name": "Mark Webber",
+        "nationality": "Australia"
+    },
+    "raikkonen": {
+        "full_name": "Kimi Räikkönen",
+        "nationality": "Finland"
+    },
+    "button": {
+        "full_name": "Jenson Button",
+        "nationality": "United Kingdom"
+    },
+    "massa": {
+        "full_name": "Felipe Massa",
+        "nationality": "Brazil"
+    },
+    "vettel": {
+        "full_name": "Sebastian Vettel",
+        "nationality": "Germany"
+    },
+    "resta": {
+        "full_name": "Paul di Resta",
+        "nationality": "United Kingdom"
+    },
+    "vergne": {
+        "full_name": "Jean-Éric Vergne",
+        "nationality": "France"
+    },
+    "petrov": {
+        "full_name": "Vitaly Petrov",
+        "nationality": "Russia"
+    },
+    "michael_schumacher": {
+        "full_name": "M. Schumacher",
+        "nationality": "Germany"
+    },
+    "grosjean": {
+        "full_name": "Romain Grosjean",
+        "nationality": "France"
+    },
+    "maldonado": {
+        "full_name": "Pastor Maldonado",
+        "nationality": "Venezuela"
+    },
+    "bruno_senna": {
+        "full_name": "Bruno Senna",
+        "nationality": "Brazil"
+    },
+    "kobayashi": {
+        "full_name": "Kamui Kobayashi",
+        "nationality": "Japan"
+    },
+    "kovalainen": {
+        "full_name": "H. Kovalainen",
+        "nationality": "Finland"
+    },
+    "glock": {
+        "full_name": "Timo Glock",
+        "nationality": "Germany"
+    },
+    "pic": {
+        "full_name": "Charles Pic",
+        "nationality": "France"
+    },
+    "ambrosio": {
+        "full_name": "J. d'Ambrosio",
+        "nationality": "Belgium"
+    },
+    "rosa": {
+        "full_name": "Pedro de la Rosa",
+        "nationality": "Spain"
+    },
+    "kubica": {
+        "full_name": "Robert Kubica",
+        "nationality": "Poland"
+    },
+    "barrichello": {
+        "full_name": "R. Barrichello",
+        "nationality": "Brazil"
+    },
+    "sutil": {
+        "full_name": "Adrian Sutil",
+        "nationality": "Germany"
+    },
+    "liuzzi": {
+        "full_name": "V. Liuzzi",
+        "nationality": "Italy"
+    },
+    "alguersuari": {
+        "full_name": "J. Alguersuari",
+        "nationality": "Spain"
+    },
+    "heidfeld": {
+        "full_name": "Nick Heidfeld",
+        "nationality": "Germany"
+    },
+    "buemi": {
+        "full_name": "Sébastien Buemi",
+        "nationality": "Switzerland"
+    },
+    "trulli": {
+        "full_name": "Jarno Trulli",
+        "nationality": "Italy"
+    },
+    "yamamoto": {
+        "full_name": "Sakon Yamamoto",
+        "nationality": "Japan"
+    },
+    "grassi": {
+        "full_name": "Lucas di Grassi",
+        "nationality": "Brazil"
+    },
+    "chandhok": {
+        "full_name": "Karun Chandhok",
+        "nationality": "India"
+    },
+    "klien": {
+        "full_name": "Christian Klien",
+        "nationality": "Austria"
+    },
+    "karthikeyan": {
+        "full_name": "N. Karthikeyan",
+        "nationality": "India"
+    },
+    "gutierrez": {
+        "full_name": "E. Gutiérrez",
+        "nationality": "Mexico"
+    },
+    "garde": {
+        "full_name": "van der Garde", 
+        "nationality": "Netherlands"
+    },
+    "chilton": {
+        "full_name": "Max Chilton",
+        "nationality": "United Kingdom"
+    },
+    "jules_bianchi": {
+        "full_name": "Jules Bianchi",
+        "nationality": "France"
+    },
+    "kvyat": {
+        "full_name": "Daniil Kvyat",
+        "nationality": "Russia"
+    },
+    "ericsson": {
+        "full_name": "Marcus Ericsson",
+        "nationality": "Sweden"
+    },
+    "stevens": {
+        "full_name": "Will Stevens",
+        "nationality": "United Kingdom"
+    },
+    "lotterer": {
+        "full_name": "André Lotterer",
+        "nationality": "Germany"
+    },
+    "nasr": {
+        "full_name": "Felipe Nasr",
+        "nationality": "Brazil"
+    },
+    "merhi": {
+        "full_name": "Roberto Merhi",
+        "nationality": "Spain"
+    },
+    "rossi": {
+        "full_name": "Alexander Rossi",
+        "nationality": "United States"
+    },
+    "vandoorne": {
+        "full_name": "Stoffel Vandoorne",
+        "nationality": "Belgium"
+    },
+    "jolyon_palmer": {
+        "full_name": "Jolyon Palmer",
+        "nationality": "United Kingdom"
+    },
+    "wehrlein": {
+        "full_name": "Pascal Wehrlein",
+        "nationality": "Germany"
+    },
+    "haryanto": {
+        "full_name": "Rio Haryanto",
+        "nationality": "Indonesia"
+    },
+    "giovinazzi": {
+        "full_name": "A. Giovinazzi",
+        "nationality": "Italy"
+    },
+    "brendon_hartley": {
+        "full_name": "Brendon Hartley",
+        "nationality": "New Zealand"
+    },
+    "sirotkin": {
+        "full_name": "Sergey Sirotkin",
+        "nationality": "Russia"
+    },
+    "latifi": {
+        "full_name": "Nicholas Latifi",
+        "nationality": "Canada"
+    },
+    "aitken": {
+        "full_name": "Jack Aitken",
+        "nationality": "United Kingdom"
+    },
+    "pietro_fittipaldi": {
+        "full_name": "P. Fittipaldi",
+        "nationality": "Brazil"
+    },
+    "mick_schumacher": {
+        "full_name": "Mick Schumacher",
+        "nationality": "Germany"
+    },
+    "mazepin": {
+        "full_name": "Nikita Mazepin",
+        "nationality": "Russia"
     }
 }
